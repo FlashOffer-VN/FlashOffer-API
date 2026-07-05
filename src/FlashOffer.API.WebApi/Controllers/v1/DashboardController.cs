c27452c87ef47f0edbdfb841f62f9f0eca7fa2e6
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2026-07-05 15:42:05 +0700

    Remove stale admin base route attribute from DashboardController

--- src/FlashOffer.API.WebApi/Controllers/v1/DashboardController.cs
+++ src/FlashOffer.API.WebApi/Controllers/v1/DashboardController.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Localization;
 
 namespace FlashOffer.API.WebApi.Controllers.Admin;
 
-[Route("api/admin")]
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/admin")]
 [Authorize(Roles = "Admin")]
