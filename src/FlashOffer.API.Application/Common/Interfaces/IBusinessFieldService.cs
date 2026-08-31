1c36bf1b6ab360d840eb0cea87215cb449585b19
quaqduy <quaqduy.it@gmail.com>
2026-08-31 22:03:56 +0700

    Revert "revert-to-deployable-version (#123)"
    This reverts commit f669d4473a9a12aa29ddaa06e62cd0b4bcc98a33.
    
    # Conflicts:
    #	src/FlashOffer.API.Application/Resources/SharedResource.en.resx
    #	src/FlashOffer.API.Application/Resources/SharedResource.vi.resx
    #	src/FlashOffer.API.Infrastructure/Migrations/20260826151423_AddBusinessFieldRelations.Designer.cs

---
+++ src/FlashOffer.API.Application/Common/Interfaces/IBusinessFieldService.cs
@@ -0,0 +1,9 @@
+﻿using FlashOffer.API.Application.DTOs.Responses;
+
+namespace FlashOffer.API.Application.Common.Interfaces;
+
+public interface IBusinessFieldService
+{
+    Task<Guid> GetOrCreateBusinessFieldAsync(string name);
+    Task<List<BusinessFieldDto>> GetActiveFieldsAsync();
+}
